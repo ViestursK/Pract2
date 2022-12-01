8050f28100ebd4494447453c15229450627f9953
Viesturs <viesturs.kalcenaus@va.lv>
2022-12-01 19:35:16 +0000

    fixed script not working

--- auto.sh
+++ auto.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 echo "Script for doing all available tests and executing program"
+
+echo "Getting python3 executable loc"
+python_exec_loc=$(which python3)
+if [ $? -eq 0 ]; then echo "All good bruv"; else echo "Master, I've run into a problem while trying to find your python exec location :("; exit 1; fi
+echo "$python_exec_loc"
+echo "------------------------" 
+
 echo "------------------------"
 if test -f "test_worker.py"; then
-    echo "test found"
+    echo "test found - test_worker"
+    $python_exec_loc test_worker.py
 else 
     echo "Missing test file..."
 fi
 echo "------------------------"
 
 if test -f "test_config.py"; then
-    echo "test found"
+    echo "test found - test_config"
+    $python_exec_loc test_config.py
 else 
     echo "Missing test file..."
 fi
 echo "------------------------"
 
 if test -f "my_test.py"; then
-    echo "test found"
+    echo "test found - my_test"
+    $python_exec_loc my_test.py
 else 
     echo "Missing test file..."
 fi
 echo "------------------------"
 
-py_exec_loc=$(which python3)
-if [$? -eq 0]; then echo "All good bruv"; else echo "Master, I've run into a problem while trying to find your python exec location :("
-echo "$py_exec_loc"
-echo "------------------------"
-
 echo "Okay, sir. You're good to go! to start the program - execute this:"
-echo "$py_exec_loc naked.py" 
+echo "$python_exec_loc naked.py" 
